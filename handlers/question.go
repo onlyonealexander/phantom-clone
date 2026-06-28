1c6a8de219de21f6c315926b22ae95f764c28502
onlyonealexander <jamesinyang01@gmail.com>
2026-06-28 16:59:32 +0100

    add request logging

--- handlers/question.go
+++ handlers/question.go
@@ -12,7 +12,7 @@ type QuestionRequest struct {
 }
 
 func SubmitQuestion(w http.ResponseWriter, r *http.Request) {
-
+     log.Println("Request received:", r.Method)
  w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
     w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
